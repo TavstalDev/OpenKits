28e418f6f535fd9a891622b4cf693ba81394361e
TavstalDev <tavstal@gmail.com>
2025-02-18 13:21:41 +0100

    Added shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
 }
 
 group = "io.github.tavstal"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
+
 
 repositories {
     mavenCentral()
@@ -12,7 +13,6 @@ repositories {
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
     maven {
-        name = "jitpack"
         url = uri("https://jitpack.io")
     }
     maven {
@@ -36,4 +36,57 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register<Jar>("mojangJar") {
+    manifest {
+        attributes["paperweight-mappings-namespace"] = "mojang"
+    }
+
+    // Set .jar name
+    archiveBaseName.set("openkits-mojang")
+
+    // Set the duplicates strategy
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // Collect runtime classpath files
+    from({
+        configurations.runtimeClasspath.get().filter { it.exists() }.map { if (it.isDirectory) it else zipTree(it) }
+    })
+
+    // Optionally, include the compiled classes
+    from(sourceSets.main.get().output)
+}
+
+tasks.register<Jar>("spigotJar") {
+    manifest {
+        attributes["paperweight-mappings-namespace"] = "spigot"
+    }
+
+    // Set .jar name
+    archiveBaseName.set("openkits-spigot")
+
+    // Set the duplicates strategy
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // Collect runtime classpath files
+    from({
+        configurations.runtimeClasspath.get().filter { it.exists() }.map { if (it.isDirectory) it else zipTree(it) }
+    })
+
+    // Optionally, include the compiled classes
+    from(sourceSets.main.get().output)
+}
+
+tasks.register("buildJars") {
+    dependsOn("mojangJar", "spigotJar")
+}
+
+tasks.named("build") {
+    dependsOn("processResources")
+    dependsOn("buildJars")
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
 }
