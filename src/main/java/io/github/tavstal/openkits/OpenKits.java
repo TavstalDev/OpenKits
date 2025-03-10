185152e149773f77d8454b7f664b6b4111fd0462
tavstal <tavstal@gmail.com>
2025-03-10 17:00:18 +0100

    Removed isUpToDate() since it already exists in the PluginBase

--- src/main/java/io/github/tavstal/openkits/OpenKits.java
+++ src/main/java/io/github/tavstal/openkits/OpenKits.java
@@ -11,26 +11,15 @@ import io.github.tavstal.openkits.managers.MySqlManager;
 import io.github.tavstal.openkits.managers.SqlLiteManager;
 import io.github.tavstal.openkits.models.IDatabase;
 import io.github.tavstal.openkits.utils.EconomyUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
 
 public class OpenKits extends PluginBase {
     public static OpenKits Instance;
+    private final PluginLogger _logger;
+    private final PluginTranslator _translator;
     public static PluginLogger Logger() {
-        return Instance.getCustomLogger();
-    }
-    public static PluginTranslator Translator() {
-        return Instance.getTranslator();
+        return Instance._logger;
     }
     private static SpiGUI _spiGUI;
     /**
@@ -57,20 +46,22 @@ public class OpenKits extends PluginBase {
                 "https://github.com/TavstalDev/OpenKits/releases/latest",
                 new String[]{"eng", "hun"}
         );
+        _logger = getCustomLogger();
+        _translator = getTranslator();
     }
 
     @Override
     public void onEnable() {
         Instance = this;
-        getCustomLogger().Info(String.format("Loading %s...", getProjectName()));
+        _logger.Info(String.format("Loading %s...", getProjectName()));
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
-            getCustomLogger().Warn("Could not find PlaceholderAPI! This plugin is required.");
+            _logger.Warn("Could not find PlaceholderAPI! This plugin is required.");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
         //new PAPIExpansion().register();
-        getCustomLogger().Info("Hooked into PlaceholderAPI.");
+        _logger.Info("Hooked into PlaceholderAPI.");
 
         // Register Events
         EventListener.init();
@@ -79,20 +70,20 @@ public class OpenKits extends PluginBase {
         saveDefaultConfig();
 
         // Load Localizations
-        if (!getTranslator().Load())
+        if (!_translator.Load())
         {
-            getCustomLogger().Error("Failed to load localizations... Unloading...");
+            _logger.Error("Failed to load localizations... Unloading...");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
 
         // Register Economy
-        getCustomLogger().Debug("Hooking into Vault...");
+        _logger.Debug("Hooking into Vault...");
         if (EconomyUtils.setupEconomy())
-            getCustomLogger().Info("Economy plugin found and hooked into Vault.");
+            _logger.Info("Economy plugin found and hooked into Vault.");
         else
         {
-            getCustomLogger().Warn("Economy plugin not found. Disabling economy features.");
+            _logger.Warn("Economy plugin not found. Disabling economy features.");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
@@ -117,11 +108,11 @@ public class OpenKits extends PluginBase {
         Database.CheckSchema();
 
         // Register GUI
-        getCustomLogger().Debug("Loading GUI...");
+        _logger.Debug("Loading GUI...");
         _spiGUI = new SpiGUI(this);
 
         // Register Commands
-        getCustomLogger().Debug("Registering commands...");
+        _logger.Debug("Registering commands...");
         var command = getCommand("kit");
         if (command != null) {
             command.setExecutor(new CommandKit());
@@ -132,15 +123,15 @@ public class OpenKits extends PluginBase {
             command.setExecutor(new CommandKits());
         }
 
-        getCustomLogger().Info(String.format("%s has been successfully loaded.", getProjectName()));
+        _logger.Info(String.format("%s has been successfully loaded.", getProjectName()));
         if (!isUpToDate())
-            getCustomLogger().Warn(String.format("A new version of %s is available! Download it at %s", getProjectName(), getDownloadUrl()));
+            _logger.Warn(String.format("A new version of %s is available! Download it at %s", getProjectName(), getDownloadUrl()));
     }
 
     @Override
     public void onDisable() {
         Database.Unload();
-        getCustomLogger().Info(String.format("%s has been successfully unloaded.", getProjectName()));
+        _logger.Info(String.format("%s has been successfully unloaded.", getProjectName()));
     }
 
     @Override
@@ -161,43 +152,12 @@ public class OpenKits extends PluginBase {
      * Reloads the plugin configuration and localizations.
      */
     public void reload() {
-        getCustomLogger().Info("Reloading OpenKits...");
-        getCustomLogger().Debug("Reloading localizations...");
-        getTranslator().Load();
-        getCustomLogger().Debug("Localizations reloaded.");
-        getCustomLogger().Debug("Reloading configuration...");
+        _logger.Info(String.format("Reloading %s...", getProjectName()));
+        _logger.Debug("Reloading localizations...");
+        _translator.Load();
+        _logger.Debug("Localizations reloaded.");
+        _logger.Debug("Reloading configuration...");
         this.reloadConfig();
-        getCustomLogger().Debug("Configuration reloaded.");
-    }
-
-    /**
-     * Checks if the plugin is up to date by comparing the current version with the latest release version.
-     * @return true if the plugin is up to date, false otherwise.
-     */
-    public boolean isUpToDate() {
-        String version;
-        getCustomLogger().Debug("Checking for updates...");
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            getCustomLogger().Debug("Sending request to GitHub...");
-            HttpGet request = new HttpGet(getDownloadUrl());
-            HttpResponse response = httpClient.execute(request);
-            getCustomLogger().Debug("Received response from GitHub.");
-            String jsonResponse = EntityUtils.toString(response.getEntity());
-            getCustomLogger().Debug("Parsing response...");
-            JSONParser parser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) parser.parse(jsonResponse);
-            getCustomLogger().Debug("Parsing release version...");
-            version = jsonObject.get("tag_name").toString();
-        } catch (IOException e) {
-            getCustomLogger().Error("Failed to check for updates.");
-            return false;
-        } catch (ParseException e) {
-            getCustomLogger().Error("Failed to parse release version.");
-            return false;
-        }
-
-        getCustomLogger().Debug("Current version: " + getVersion());
-        getCustomLogger().Debug("Latest version: " + version);
-        return version.equalsIgnoreCase(getVersion());
+        _logger.Debug("Configuration reloaded.");
     }
 }
